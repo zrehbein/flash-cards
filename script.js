438009aafd4c8cb1685ed577ef57a749dbb45d9e
zrehbein <zrehbein@gmail.com>
2021-08-02 16:51:11 -0700

    images loaded

--- script.js
+++ script.js
@@ -2,9 +2,14 @@
 
 //get elements from html
 //"cards", hidden values, player scores
+let cards = document.querySelectorAll(".card")
 
 //click on cards to reveal hidden value
 
+//remove correctly chosen cards from the board
+
 //reset: randomize location of hidden values
 
+//*add a point tracker function
 
+//*add animation and sounds on click
