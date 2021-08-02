aa030502061a0f17e54b958aed0f586a72b0440c
zrehbein <zrehbein@gmail.com>
2021-08-01 22:59:11 -0700

    framework, pseudocode

---
+++ script.js
@@ -0,0 +1,10 @@
+//functions to be built
+
+//get elements from html
+//"cards", hidden values, player scores
+
+//click on cards to reveal hidden value
+
+//reset: randomize location of hidden values
+
+
